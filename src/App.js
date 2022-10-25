09443fe3b08d8764179f03622299ca263f425df0
suedacicekli <suedacicekli@gmail.com>
2022-10-25 23:23:03 +0300

    Routing | react-router-dom and useNavigate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//pages
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+import Contact from "./pages/Contact";
+//components
+import Menu from "./components/Menu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="users" element={<Users />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Menu.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+function Menu() {
+
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <ul className="menu">
+                <li className="cursor" onClick={() => navigate(-1)}>Geri</li>
+                {/* <li><a href="/">Anasayfa(test)</a></li> */}
+                <li><Link to="/">Anasayfa</Link></li>
+                <li><Link to="users">Kullanıcılar</Link></li>
+                <li><Link to="contact">İletişim</Link></li>
+                <li className="cursor" onClick={() => navigate("contact")}>İletişim</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu
